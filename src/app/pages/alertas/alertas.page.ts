c18f2e7ed2277b1713e9f7fb8e58b982bba0632d
alejandrossm <alejandrossm@outlook.com>
2024-09-12 09:12:58 -0300

    Alertas ok

--- src/app/pages/alertas/alertas.page.ts
+++ src/app/pages/alertas/alertas.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alertas',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertasPage implements OnInit {
 
-  constructor() { }
+  alertButtons=[{
+    text:'Aceptar',
+    cssClass:'color-aceptar'
+  },{
+    text:'Cancelar',
+    cssClass:'color-cancelar'
+  }]
+  constructor(private alertctrl:AlertController) { }
 
   ngOnInit() {
   }
 
+  async alerta(event:any){
+    console.log("Alerta desde controller");
+    const alert = await this.alertctrl.create({
+      header: 'Alerta con Controller',
+      subHeader: 'A Sub Header Is Optional',
+      message: 'A message should be a short, complete sentence.',
+      buttons: [{
+        id:'aceptar del alert controller',
+        text:'Aceptar',
+        cssClass:'color-aceptar',
+        handler:()=>{
+          console.log(event);
+        }
+      },{
+        text:'Cancelar',
+        cssClass:'color-cancelar'
+      }],
+    });
+
+    await alert.present();
+  }
+  
 }
